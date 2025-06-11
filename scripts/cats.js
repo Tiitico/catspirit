884115c76e7c6efb644eaa94bc7dbbe3975079e8
Tiitico <thiagofinatti2008@gmail.com>
2025-06-11 15:37:41 -0300

    Update cat descriptions with detailed information and fun facts

--- scripts/cats.js
+++ scripts/cats.js
@@ -6,7 +6,7 @@ const cats =[
         run3: './public/sources/cats/blackcatrun3.png', 
         image: './public/sources/cats/blackcat.png',
         squat:'./public/sources/cats/blackcatsquat.png',
-        description: 'um gato preto qualquer' ,
+        description: 'gatos pretos, por conta de sua cor absolutamente escura, possuem uma alta facilidade de se camuflar, possuindo até mesmo memes mostrando um gato preto surgindo do completo breu, graças a esse traço genetico, gatos pretos vivem em completa tranquilidade, sabendo que predadores dificilmente os enxergarão, apesar de crenças apontarem que são seres que trazem azar, gatos pretos são um dos tipos mais doceis e amigaveis de gatos, possuindo uma sorte natural de ter uma pelagem tão linda!! ' ,
         meme:'Eu quando.. eu quando uuuh.. ééééé.. MENTIRAS, MENTIRAS FALSIDADES, eu sou um ESPALHADOR DE DESINFORMAÇÃO, eu gosto de PROPAGAR FATOS IRREAIS PELA INTERNET'
     },
 
@@ -17,7 +17,7 @@ const cats =[
         run3: './public/sources/cats/orangecatrun3.png', 
         image: './public/sources/cats/orangecat.png',
         squat:'./public/sources/cats/orangecatsquat.png',
-        description: 'não confie nele.',
+        description: 'não confie nele. <br>fora a piada a parte, gatos laranjas são extremamente doceis por motivos similares ao dos gatos pretos, apesar de que para nós humanos, os gatos laranjas tenham uma cor laranja, para a maioria dos predadores e presas na natureza, esses gatos na verdade são verdes igual a mata, isso se da pelo fato de que a maioria dos seres-vivos não enxergam a cor vermelha. Como a composição da luz laranja é formada por espectros de verde e vermelho, o gato acaba tendo a coloraçâo puxada para um verde acizentado para aqueles que náo vem o vermelho. ',
         meme:'ele está atras de pedro.'
     },
     {
@@ -47,7 +47,7 @@ const cats =[
         run3: './public/sources/cats/whitecatrun3.png', 
         image: './public/sources/cats/whitecat.png',
         squat:'./public/sources/cats/whitecatsquat.png',
-        description: 'ele tem heterocromia nos olhos :O',
+        description: 'ele tem heterocromia nos olhos :O<br> gatos totalmente brancos com olhos azuis, tem a chance de serem surdos no lado do olho azul, por conta da formação genetica dos olhos azuis em gatos albinos, que por sua vez podem cancelar o processo de criação importante da função auditiva do gato.',
         meme:'muehehehehee >:)'
     },
     {
@@ -57,7 +57,7 @@ const cats =[
         run3: './public/sources/cats/frajolacatrun3.png', 
         image:'./public/sources/cats/frajolacat.png',
         squat:'./public/sources/cats/frajolacatsquat.png',
-        description: 'isso era pra ser um frajola aaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa  aaaaaaaaaaaa  aaaaaaaaaaam aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaaaaa aaaaaaaaa  aaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaa aaaaaaaaaaaaaa aaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaa mas ta mais pra um gato branco com preto',
+        description: 'o nome frajola dos gatos preto e branco se da pelo fato de que, o real significado dessa palavra seria de alguem elegante, uma pessoa que se veste apenas com roupas de gala, e como gatos frajolas tendem a ter suas costas pretas, patas, rabo, e cabeça, parece ser um gato de terno, um belo e natural terno.',
         meme:'balerina capuccina mi mi mii'
     },
     {
@@ -67,7 +67,7 @@ const cats =[
         run3: './public/sources/cats/browncatrun3.png', 
         image:'./public/sources/cats/browncat.png',
         squat:'./public/sources/cats/browncatsquat.png',
-        description: 'marrom aaaaaaaaa',
+        description: 'o gato marrom na verdade é como um gato preto, sua pelagem é uma diluição da cor preta causada por um gene recessivo, o tornando um gato extremamente raro, principalmente gatos de pelagem marrom mais claros, é mais comum encontrar gatos que possuem pelos pretos, porem com uma leve coloração de marrom, em si, gatos marrons são doceis assim como gatos pretos.',
         meme:'ele tem uma bomba.'
     },
     {
@@ -77,7 +77,7 @@ const cats =[
         run3: './public/sources/cats/graycatrun3.png', 
         image:'./public/sources/cats/graycat.png',
         squat:'./public/sources/cats/graycatsquat.png',
-        description: 'cinza aaaaaaaaa',
+        description: 'gatos cinzas assim como os marrons, tambem possuem pelagens pretas diluidas, sendo um caso não tão raro como gatos marrons, são muito comuns na russia e são conhecidos como "gatos azuis",por conta que sua pelagem pode ser confundida com um tom leve de azul, mas ele é cinza!',
         meme:'engualzinho eu kkkjjjk.'
     },
     {
@@ -87,7 +87,7 @@ const cats =[
         run3: './public/sources/cats/siamescatrun3.png', 
         image:'./public/sources/cats/siamescat.png',
         squat:'./public/sources/cats/siamescatsquat.png',
-        description: 'nao sei oq por',
+        description: 'gatos siameses existem em dois tipos, os gatos de pelagem siamesa, e os reais siameses, esses sendo originais da tailandia, geralmente gatos siameses nascem com uma pelagem similar ao branco, mas conforme crescem, sua cor escurece, isso se da por que a pelagem desses gatos funciona de forma diferente dos demais, pontos de maior calor no gato geralmente são os lugares mais escuros de sua pelugem, como por exemplo suas orelhas, patas, caudas e rosto. Podendo mudar de cor dependendo da temperatura de seu corpo, isso ajuda o gato a se proteger em ambientes quentes ou frios, sendo um gato resistente a qualquer mudança de clima.',
         meme:'gato de gravata pomposo.'
     },
 ]
